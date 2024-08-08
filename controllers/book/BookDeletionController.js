6b5f5855e821e0220d49d00809d1561cd1c391b7
Alaa-Eldeen <alaaamin706@gmail.com>
2024-08-08 14:34:33 +0300

    Book deletion controller

---
+++ controllers/book/BookDeletionController.js
@@ -0,0 +1,26 @@
+class BookDeletionController {
+  constructor(bookDeletionService) {
+    this.bookDeletionService = bookDeletionService;
+  }
+
+  async deleteBook(req, res) {
+    try {
+      const book = await this.bookDeletionService.deleteBook(req.params.bookId);
+      if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+
+      return req
+        .status(200)
+        .json({ message: "Book deleted successfully", book });
+    } catch (err) {
+      console.error("Error deleting book:", err);
+      res.status(500).json({
+        message:
+          "An error occurred while deleting the book. Please try again later.",
+      });
+    }
+  }
+}
+
+module.exports = BookDeletionController;
