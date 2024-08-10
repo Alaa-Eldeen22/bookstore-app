9f843b9910021bff7024e568801d992c4eded40e
Alaa-Eldeen <alaaamin706@gmail.com>
2024-08-10 14:34:15 +0300

    Create review model

---
+++ models/Review.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    required: true,
+  },
+  rating: {
+    type: Number,
+  },
+  comment: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
