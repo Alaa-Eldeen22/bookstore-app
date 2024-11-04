2bcb808f034a1a120e4daaabca8acc34fbebc114
Alaa-Eldeen <alaaamin706@gmail.com>
2024-11-04 16:42:32 +0200

    Modify response for cart retrieval

--- services/cart/CartRetrievalService.js
+++ services/cart/CartRetrievalService.js
@@ -1,3 +1,25 @@
+// class CartRetrievalService {
+//   constructor(CartModel) {
+//     this.CartModel = CartModel;
+//   }
+
+//   async getCart(userId) {
+//     const cart = await this.CartModel.findOne({ user: userId }).populate({
+//       path: "items.book",
+//       select: "name price image",
+//     });
+
+//     if (!cart) {
+//       const error = new Error("Cart not found for this user.");
+//       error.statusCode = 404;
+//       throw error;
+//     }
+//     console.log(cart);
+//     return cart;
+//   }
+// }
+
+// module.exports = CartRetrievalService;
 class CartRetrievalService {
   constructor(CartModel) {
     this.CartModel = CartModel;
@@ -15,7 +37,17 @@ class CartRetrievalService {
       throw error;
     }
 
-    return cart;
+    // Map the items array to only include the desired fields
+    const simplifiedCart = cart.items.map((item) => ({
+      bookId: item.book._id.toString(),
+      name: item.book.name,
+      price: item.book.price,
+      image: item.book.image,
+      quantity: item.quantity,
+    }));
+
+    console.log(simplifiedCart);
+    return simplifiedCart;
   }
 }
 
