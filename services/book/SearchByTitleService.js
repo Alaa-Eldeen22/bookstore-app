6dae48d497074701c472bf2b4c0b022e08bf9ff0
Alaa-Eldeen <alaaamin706@gmail.com>
2024-11-17 04:29:43 +0200

    Modify search response

--- services/book/SearchByTitleService.js
+++ services/book/SearchByTitleService.js
@@ -4,9 +4,21 @@ class SearchByTitleService {
   }
 
   async searchByTitle(title) {
-    return await this.BookModel.find({
+    const books = await this.BookModel.find({
       name: { $regex: title, $options: "i" },
     });
+
+    return books.map((book) => ({
+      bookId: book._id.toString(),
+      name: book.name,
+      category: book.category,
+      authors: book.authors,
+      numberOfPages: book.numberOfPages,
+      description: book.description,
+      price: book.price,
+      quantity: book.quantity,
+      image: book.image,
+    }));
   }
 }
 
